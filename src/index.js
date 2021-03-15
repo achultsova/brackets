b5c2000e28dce05f5b81b60846a6d89046676b31
Anastasiya <nchulczova@gmail.com>
2021-03-15 18:34:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const open = ["(", "{", "["];
+  const close = [")", "}", "]"];
+  const same = ["|"];
+
+  bracketsConfig.forEach(function (arr) {
+      if (arr[0] != arr[1]) {
+          open.push(arr[0]);
+          close.push(arr[1]);
+      } else {
+          same.push(arr[0]);
+      }
+  });
+
+  let stack = [];
+  let z = str.toString().split("");
+  for (let i = 0; i < z.length; i++) {
+      if (open.includes(z[i])) {
+          stack.push(z[i]);
+      } else if (close.includes(z[i])) {
+          if (open.indexOf(stack[stack.length - 1]) == close.indexOf(z[i])) {
+              stack.pop();
+          } else {
+              return false;
+          }
+      } else if (same.includes(z[i])) {
+          if (stack[stack.length - 1] == z[i]) {
+              stack.pop();
+          } else if (!stack.includes(z[i])) {
+              stack.push(z[i]);
+          } else {
+              return false;
+          }
+      } else {
+          return false;
+      }
+  }
+  return stack.length == 0 ? true : false;
+};
